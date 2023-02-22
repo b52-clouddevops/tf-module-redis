493960d30387e30902fa74b2473d787e70653436
CodingManoj <mvp2612@hotmail.com>
2023-02-21 20:13:17 -0500

    Auto Push Commit

--- redis.tf
+++ redis.tf
@@ -18,7 +18,7 @@ resource "aws_elasticache_parameter_group" "redis-pg" {
 
 # Creates Subnet Group 
 resource "aws_elasticache_subnet_group" "redis-sg" {
-  name       = "tf-test-cache-subnet"
+  name       = "roboshop-${var.ENV}-redis-sg"
   subnet_ids = [aws_subnet.foo.id]
 }
 
