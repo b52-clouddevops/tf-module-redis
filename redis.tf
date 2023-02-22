cf18760803f198bc7a935a041a6d9cbbd3e665a4
CodingManoj <mvp2612@hotmail.com>
2023-02-21 20:20:34 -0500

    Auto Push Commit

--- redis.tf
+++ redis.tf
@@ -7,7 +7,7 @@ resource "aws_elasticache_cluster" "redis" {
   num_cache_nodes      = 1
   parameter_group_name = aws_elasticache_parameter_group.redis-pg.name
   subnet_group_name    = aws_elasticache_subnet_group.redis-sg.name
-  security_group_ids   = [aws_security_group.allow_redis.id
+  security_group_ids   = [aws_security_group.allow_redis.id]
   engine_version       = "6.2"
   port                 = 6379
 }
